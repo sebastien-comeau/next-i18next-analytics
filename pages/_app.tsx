dae943b1e1a191597cd4863961c39037cb7edce1
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2023-06-21 09:41:07 -0300

    add analytics reproducible example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,51 @@
-import type { AppProps } from 'next/app'
-import { appWithTranslation } from 'next-i18next'
+import type { AppProps } from 'next/app';
+import { appWithTranslation } from 'next-i18next';
+import { FC, useEffect } from 'react';
+import { useRouter } from 'next/router';
 // import nextI18NextConfig from '../next-i18next.config.js'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-)
+// help to prevent double firing of analytics pageLoad event
+let appPreviousLocationPathname = '';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  // Web Analytics - taken from Google Analytics example
+  // @see https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      // only push event if pathname is different
+      if (window.location.pathname !== appPreviousLocationPathname) {
+        appPreviousLocationPathname = window.location.pathname;
+
+        console.log({
+          url,
+          appPreviousLocationPathname,
+          pathname: window.location.pathname,
+          title: document.title,
+        });
+
+        setTimeout(() => {
+          console.log({
+            action: 'timeout',
+            url,
+            appPreviousLocationPathname,
+            pathname: window.location.pathname,
+            title: document.title,
+          });
+        }, 500);
+      }
+    };
+
+    handleRouteChange(window.location.pathname);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
 
 // https://github.com/i18next/next-i18next#unserializable-configs
-export default appWithTranslation(MyApp /*, nextI18NextConfig */)
+export default appWithTranslation(MyApp /*, nextI18NextConfig */);
